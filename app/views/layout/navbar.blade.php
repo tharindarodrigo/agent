85e53abb3ccda4c241a49e449812a19b161db0ce
Tharinda <tharindarodrigo@gmail.com>
2016-02-01 10:34:47 +0530

    Merge branch 'master' of C:\wamp\www\agent with conflicts.

--- app/views/layout/navbar.blade.php
+++ app/views/layout/navbar.blade.php
@@ -24,83 +24,48 @@
             </ul>
 
         </li>
-
-        @if(Session::has('rate_box_details'))
-            <?php
-            $total_hotel_amount = 0;
-            $bookings = Session::get('rate_box_details');
-            $hotel_bookings = [];
-            // dd($bookings);
-
-            $rate_keys = array_keys($bookings);
-
-            foreach ($rate_keys as $rate_key) {
-                $hotel_id = explode('_', $rate_key)[0];
-
-                $hotel_bookings[$hotel_id][] = $bookings[$rate_key];
-                $hotel_bookings[$hotel_id]['hotel_name'] = $bookings[$rate_key]['hotel_name'];
-                $hotel_bookings[$hotel_id]['hotel_address'] = $bookings[$rate_key]['hotel_address'];
-                $hotel_bookings[$hotel_id]['room_identity'] = $bookings[$rate_key]['room_identity'];
-            }
-            ?>
-            <li class="dropdown">
-                <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
-                    <i class="fa fa-shopping-cart"></i>
-                    <span class="label label-primary">
-                        {{ count(Session::get('rate_box_details'))  }}
-                    </span>
-                </a>
-
-                <ul class="dropdown-menu dropdown-alerts">
-                    @foreach($hotel_bookings as $booking)
-                        <?php  $total_cost = 0; ?>
-                        <li>
-                            <a href="{{URL::to('/booking-cart')}}">
-                                <div>
-                                    <i class="fa fa-envelope fa-fw"></i> {{ $booking['hotel_name'] }}
-                                    @for($c=0 ; $c < count($booking)-3; $c++)
-                                        <?php $total_cost = $total_cost + $booking[$c]['room_cost'] + ($booking[$c]['hotel_tax'] + $booking[$c]['hotel_handling_fee'] + $booking[$c]['supplement_rate']); ?>
-                                    @endfor
-                                    <span style="color: #1ab394" class="pull-right text-muted small">
-                                        USD {{  number_format(($total_cost), 2, '.', '')  }}
-                                    </span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <?php $total_hotel_amount = $total_hotel_amount + $total_cost ?>
-                    @endforeach
-                    <li>
-                        <div class="text-center link-block">
-                            <span style="color: #1ab394">Total : USD {{  number_format(($total_hotel_amount), 2, '.', '')  }}</span>
-
-                            <a href="{{URL::to('/booking-cart')}}">
-                                <i class="fa-shopping-cart"></i>
-                                <strong>View Cart</strong>
-                            </a>
+        <li class="dropdown">
+            <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
+                <i class="fa fa-shopping-cart"></i> <span class="label label-primary">8</span>
+            </a>
+            <ul class="dropdown-menu dropdown-alerts">
+                <li>
+                    <a href="mailbox.html">
+                        <div>
+                            <i class="fa fa-envelope fa-fw"></i> You have 16 messages
+                            <span class="pull-right text-muted small">4 minutes ago</span>
                         </div>
-                    </li>
-                </ul>
-            </li>
-
-        @else
-            <li class="dropdown">
-                <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
-                    <i class="fa fa-shopping-cart"></i> <span class="label label-primary">8</span>
-                </a>
-                <ul class="dropdown-menu dropdown-alerts">
-                    <li>
-                        <a href="mailbox.html">
-                            <div>
-                                <i class="fa fa-envelope fa-fw"></i> You have 16 messages
-                                <span class="pull-right text-muted small">4 minutes ago</span>
-                            </div>
+                    </a>
+                </li>
+                <li class="divider"></li>
+                <li>
+                    <a href="profile.html">
+                        <div>
+                            <i class="fa fa-twitter fa-fw"></i> 3 New Followers
+                            <span class="pull-right text-muted small">12 minutes ago</span>
+                        </div>
+                    </a>
+                </li>
+                <li class="divider"></li>
+                <li>
+                    <a href="grid_options.html">
+                        <div>
+                            <i class="fa fa-upload fa-fw"></i> Server Rebooted
+                            <span class="pull-right text-muted small">4 minutes ago</span>
+                        </div>
+                    </a>
+                </li>
+                <li class="divider"></li>
+                <li>
+                    <div class="text-center link-block">
+                        <a href="notifications.html">
+                            <strong>See All Alerts</strong>
+                            <i class="fa fa-angle-right"></i>
                         </a>
-                    </li>
-                    <li class="divider"></li>
-                </ul>
-            </li>
-        @endif
+                    </div>
+                </li>
+            </ul>
+        </li>
 
         <li>
             <a href="{{URL::route('account-sign-out')}}">
